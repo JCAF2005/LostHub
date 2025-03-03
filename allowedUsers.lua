ec59bf2ddec7e2ff4ee751b27b56ded52f6f0296
JOFER223 <129844041+JCAF2005@users.noreply.github.com>
2025-03-03 21:13:38 +0000

    Update allowedUsers.lua

--- allowedUsers.lua
+++ allowedUsers.lua
@@ -3,17 +3,5 @@ local allowedUsers {}
 -- User Authentication Configuration
 local allowedUsers = {3925482109, 470684482}
 
-local function isUserAllowed()
-    for _, id in ipairs(allowedUsers) do
-        if userId == id then
-            return true
-        end
-    end
-    return false
-end
-
-if not isUserAllowed() then
-    return
-end
 
 return allowedUsers
